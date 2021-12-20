c5e46c837fed2bc3efc6c3d6c8f5408336b7df8d
EricDominici <79219282+EricDominici@users.noreply.github.com>
2021-12-20 02:14:20 -0500

    Update and rename ejer20.js to ejercicio20.js

--- ejercicio20.js
+++ ejercicio20.js
@@ -4,27 +4,24 @@ siguiente, 5 en el siguiente etc. Realice una función para cada uno de  los pun
 (b) ¿Cuántas latas sobran haciendo el triángulo si se tienen n latas (con 13 latas se tienen 3  niveles y 
 sobran 4)? */
 
-var arr =[1,3,5,7,9,11,13];
-	 var num = arr.length;
-	 var car ="[]";
-   var max = Math.max(...arr);
+let arreglo =[1,3,5,7,9,11,13], num = arreglo.length;
+let c ="[]";
+let max = Math.max(...arreglo);
    console.log(max);
-   var graf = 0;
-   var MAXLENTCONTS= 14;
-
-   var print = (n) => {
-     var i=1;
+let gra = 0, maxlen= 14;
+let print = (n) => {
+    let i=1;
      while(i <= n){
-      document.write("[]");
+      console.log("[]");
       ++i;
      }
-     document.write("<br>")
+     console.log("<br>")
    }
 
-	 for(var contador=0; contador<num; ++contador){
+	 for(let contador=0; contador<num; ++contador){
 
-    graf = (arr[contador]*MAXLENTCONTS)/max;
-    graf = Math.floor(graf);
-    print(graf);
+    gra = (arreglo[contador]*maxlen)/max;
+    gra = Math.floor(gra);
+    print(gra);
     
 }
